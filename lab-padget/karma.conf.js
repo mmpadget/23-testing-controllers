311b2e2cf59e41430b702b08805e8a4837285ad6
Michael Padget <mpadget@comcast.net>
2017-05-25 11:39:22 -0700

    Fixed jasmine core issue

--- lab-padget/karma.conf.js
+++ lab-padget/karma.conf.js
@@ -6,7 +6,7 @@ delete webpack.entry;
 
 module.exports = function(config) {
   config.set({
-
+    webpack,
     // base path that will be used to resolve all patterns (eg. files, exclude)
     basePath: '',
 
